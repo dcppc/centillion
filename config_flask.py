8cca4b2c8d3d1fc1240291451d9456c3dfe81e36
Charles Reid <charlesreid1@gmail.com>
2018-08-01 00:49:56 -0700

    add TAGLINE param

--- config_flask.py
+++ config_flask.py
@@ -4,6 +4,8 @@ INDEX_DIR = "search_index"
 # Toggle to show Whoosh parsed query
 SHOW_PARSED_QUERY=True
 
+TAGLINE = "Search all the things"
+
 # Flask settings
 DEBUG = True
 SECRET_KEY = '42c5a8eda356ca9d9c3ab2d149541e6b91d843fa'
