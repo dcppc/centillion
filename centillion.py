1eaaa320076911f0e82403c5a7b2a3a71a19daef
Charles Reid <charlesreid1@gmail.com>
2018-08-09 17:25:09 -0700

    index all github filenames, not just markdown

--- centillion.py
+++ centillion.py
@@ -43,8 +43,8 @@ class UpdateIndexTask(object):
         from get_centillion_config import get_centillion_config
         config = get_centillion_config('config_centillion.json')
 
+        search.update_index_ghfiles(self.gh_access_token,config)
         search.update_index_issues(self.gh_access_token,config)
-        search.update_index_markdown(self.gh_access_token,config)
         search.update_index_gdocs(config)
 
 
