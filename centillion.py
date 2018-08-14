d193e8bf90028d087345f86075ef9373b4e60164
Charles Reid <charlesreid1@gmail.com>
2018-08-14 14:03:55 -0700

    successfully adding email threads to search index. now just UI stuff.

--- centillion.py
+++ centillion.py
@@ -49,7 +49,7 @@ class UpdateIndexTask(object):
         from get_centillion_config import get_centillion_config
         config = get_centillion_config('config_centillion.json')
 
-        search.update_index_groupsioemails(self.groupsio_credentials,config)
+        search.update_index_emailthreads(self.groupsio_credentials,config)
         ###search.update_index_ghfiles(self.gh_token,config)
         ###search.update_index_issues(self.gh_token,config)
         ###search.update_index_gdocs(config)
