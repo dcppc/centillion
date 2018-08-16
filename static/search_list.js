f958b6cf5f89e54f8e8e9e7ad43d8d19992246a6
Charles Reid <charlesreid1@gmail.com>
2018-08-16 16:18:51 -0700

    Updating search results page to be sortable
    - present search results in table
    - add javascript file for results table
    - apply dataTable plugin to results table
    - get sortable results

---
+++ static/search_list.js
@@ -0,0 +1,34 @@
+//////////////////////////////////
+// Centillion Search Results Listing
+// Javascript Functions 
+//
+// This file contains javascript functions used by
+// the search results page centillion.
+
+
+
+//////////////////////////////////
+// Results-to-DataTable Functions
+//
+// These functions post-process the table of search results
+// and make it into a dataTable.
+//
+// The dataTable bootstrap plugin is used to make the tables
+// sortable, searchable, and slick.
+
+
+$(document).ready(function() {
+    // Construct names of id tags
+    table_id = "#search-results";
+
+    // Initialize the DataTable
+    $(table_id).DataTable({
+        responsive: true,
+        searching: false,
+        order: [[0,'desc']],
+        lengthMenu: [50,100,250,500]
+    });
+
+    console.log('Finished loading search results list');
+});
+
