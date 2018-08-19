685058545a17feb3ff5ae224c5911bc0a390dd1c
Charles Reid <charlesreid1@gmail.com>
2018-08-19 01:37:55 -0700

    feedback button successfully triggers a modal

---
+++ static/feedback.js
@@ -0,0 +1,30 @@
+function add_modal() {
+    console.log('ohai');
+
+    var modal = `<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>        
+    `
+    
+    $('#master-div').prepend(modal);
+}
+
+function delete_modal() {
+    $('#master-div').find("#feedback-modal-div").remove();
+}
+
