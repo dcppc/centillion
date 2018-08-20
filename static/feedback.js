da62a5c8876518a597e36f3599c170d9f0456fc9
Charles Reid <charlesreid1@gmail.com>
2018-08-20 14:10:20 -0700

    add successful post call and export to JSON db

--- static/feedback.js
+++ static/feedback.js
@@ -7,19 +7,107 @@
 // flask post data as json:
 // https://stackoverflow.com/a/16664376
 
-/* make the smile green */
+
+/* this function is called when the user submits
+ * the feedback form. it submits a post request
+ * to the flask server, which squirrels away the
+ * feedback in a file.
+ */
+function submit_feedback() {
+    // this function is called when submit button clicked
+    // algorithm:
+    // - check if text box has content
+    // - check if happy/sad filled out
+
+    var smile_active = $('#modal-feedback-smile-div').hasClass('smile-active');
+    var frown_active = $('#modal-feedback-frown-div').hasClass('frown-active');
+    if( !( smile_active || frown_active ) ) {
+        alert('Please pick the smile or the frown.')
+    } else if( $('#modal-feedback-textarea').val()=='' ) {
+        alert('Please provide us with some feedback.')
+    } else {
+        console.log('ready to post form.');
+
+        var user_sentiment = '';
+        if(smile_active) {
+            user_sentiment = 'smile';
+        } else {
+            user_sentiment = 'frown';
+        }
+        var escaped_text = $('#modal-feedback-textarea').val();
+
+        var data = {
+            sentiment : user_sentiment,
+            content : escaped_text
+        };
+        console.log(data);
+        $.post("/feedback", 
+            data, 
+            function() {
+                console.log('ohai');
+                $('#myModal').modal('hide');
+                $('#myModalForm')[0].reset();
+                //$('#modal-feedback-textarea').reset();
+                frown_unclick();
+                smile_unclick();
+        });
+        // - $.post()
+        // - callback function resets the form 
+
+    }
+}
+
+
+/* for those particularly wordy users... limit feedback to 1000 chars */
+function cool_it() { 
+    if($('#modal-feedback-textarea').val().length > 1100 ){
+        $('#modal-too-long').show();
+    } else {
+        $('#modal-too-long').hide();
+    }
+}
+
+/* smiley functions */
 function smile_click() {
     $('#modal-feedback-smile-div').addClass('smile-active');
     $('#modal-feedback-smile-icon').addClass('smile-active');
+}
+function frown_click() {
+    $('#modal-feedback-frown-div').addClass('frown-active');
+    $('#modal-feedback-frown-icon').addClass('frown-active');
+}
+function smile_unclick() {
+    $('#modal-feedback-smile-div').removeClass('smile-active');
+    $('#modal-feedback-smile-icon').removeClass('smile-active');
+}
+function frown_unclick() {
     $('#modal-feedback-frown-div').removeClass('frown-active');
     $('#modal-feedback-frown-icon').removeClass('frown-active');
 }
 
-/* make the frown red */
-function frown_click() {
-    $('#modal-feedback-smile-div').removeClass('smile-active');
-    $('#modal-feedback-smile-icon').removeClass('smile-active');
-    $('#modal-feedback-frown-div').addClass('frown-active');
-    $('#modal-feedback-frown-icon').addClass('frown-active');
+function smile() {
+    frown_unclick();
+    smile_click();
+}
+function frown() { 
+    smile_unclick();
+    frown_click();
 }
 
+
+/* for those particularly wordy users... limit feedback to 1100 chars */
+// how to check n characters in a textarea
+// https://stackoverflow.com/a/19934613
+/*
+$(document).ready(function() {
+
+    $('#modal-feedback-textarea').on('change',function(event) {
+        if($('#modal-feedback-textarea').val().length > 1100 ){
+            $('#modal-too-long').show();
+        } else {
+            $('#modal-too-long').hide();
+        }
+    });
+
+}
+*/
