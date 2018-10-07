b8d34cb240bde4adb3e86907e3cd3cbf8b566738
C Reid <charlesreid1@Gmail.com>
2018-10-07 14:05:57 -0700

    fix flask blueprint handling, add in correct routes

--- src/webapp/flask_routes.py
+++ src/webapp/flask_routes.py
@@ -1,13 +1,20 @@
 from .const import base, call
 
+from ..search import Search
+
+from werkzeug.contrib.fixers import ProxyFix
 from flask import Flask, request, abort, render_template
-import os, sys
+from flask import Markup
+from flask_dance.contrib.github import make_github_blueprint, github
+
+import os
+import sys
 import json
 import logging
 
 
 """
-Flask Routes:
+Centillion Flask Routes:
 
 Call the method that decorates the Flask app
 with all routes.
@@ -16,54 +23,348 @@ with all routes.
 
 def setup_routes(app):
 
+    client_id = app.config['GITHUB_OAUTH_CLIENT_ID']
+    client_secret = app.config['GITHUB_OAUTH_CLIENT_SECRET']
 
-    #################
-    # TODO: FIX THIS
+    # Make the github login blueprint
+    github_bp = make_github_blueprint(
+                            client_id = client_id,
+                            client_secret = client_secret,
+                            scope='read:org')
 
-    @app.route('/', methods=['GET', 'POST'])
+    app.register_blueprint(github_bp, url_prefix="/login")
+
+    last_searches_file = app.config["INDEX_DIR"] + "/last_searches.txt" 
+
+    ##############################
+    # Flask routes
+
+    @app.route('/')
     def index():
+        if not github.authorized:
+            return render_template("landing.html")
+        else:
+            username = github.get("/user").json()['login']
+            resp = github.get("/user/orgs")
+            if resp.ok:
     
-        # forgot to add the dang render template handler
-        if request.method=='GET':
-            return render_template("index.html")
+                # If they are in dcppc, redirect to search.
+                # Otherwise, hit em with a 403
+                all_orgs = resp.json()
+                for org in all_orgs:
+                    if org['login']=='dcppc':
+                        # Business as usual
+                        return redirect(url_for("search", query="", fields=""))
     
-        config = app.config
+                # Not in dcppc
+                return render_template('403.html')
     
-        # Verify webhooks are from github
-        verify_github_source(config)
+            # Could not reach Github
+            return render_template('404.html')
     
-        # Play ping/pong with github
-        event = request.headers.get('X-GitHub-Event')
-        if event == 'ping':
-            return json.dumps({'msg': 'pong'})
     
-        # Get the payload
-        payload = get_payload(request)
+    @app.route('/log_in')
+    def log_in():
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        else:
+            username = github.get("/user").json()['login']
+            resp = github.get("/user/orgs")
+            if resp.ok:
     
-        # Enforce secret
-        enforce_secret(config,request)
+                # If they are in dcppc, redirect to search.
+                # Otherwise, hit em with a 403
+                all_orgs = resp.json()
+                for org in all_orgs:
+                    if org['login']=='dcppc':
+                        # Business as usual
+                        return redirect(url_for("search", query="", fields=""))
     
-        # Get the branch
-        branch = get_branch(payload)
+                # Not in dcppc
+                return render_template('403.html')
     
-        # Current events almost always have a repository.
-        # Be safe in case they do not.
-        name = payload['repository']['name'] if 'repository' in payload else None
+            # Could not reach Github
+            return render_template('404.html')
     
-        # Assemble quick lookup info
-        meta = {
-            'name': name,
-            'branch': branch,
-            'event': event
-        }
     
-        app.init_payload_handler()
-        payload_handler = app.get_payload_handler()
-        payload_handler.process_payload(payload, meta, config)
-        app.del_payload_handler()
     
-        # Clean up
-        return json.dumps({'status':'done'})
+    @app.route('/search')
+    def search():
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        username = github.get("/user").json()['login']
+        resp = github.get("/user/orgs")
+        if resp.ok:
+    
+            # If they are in dcppc, show them search.html
+            # Otherwise, hit em with a 403
+            all_orgs = resp.json()
+            for org in all_orgs:
+                if org['login']=='dcppc':
+                    # Business as usual
+                    query = request.args['query']
+                    fields = request.args.get('fields')
+                    if fields == 'None':
+                        fields = None
+    
+                    search = Search(app.config["INDEX_DIR"])
+                    if not query:
+                        parsed_query = ""
+                        result = []
+    
+                    else:
+                        parsed_query, result = search.search(query.split(), fields=[fields])
+                        store_search(query,fields)
+    
+                    totals = search.get_document_total_count()
+    
+                    return render_template('search.html', 
+                                           entries=result, 
+                                           query=query, 
+                                           parsed_query=parsed_query, 
+                                           fields=fields, 
+                                           totals=totals)
+    
+            # Not in dcppc 
+            return render_template('403.html')
+    
+        # Could not reach Github
+        return render_template('404.html')
+    
+    
+    @app.route('/update_index/<run_which>')
+    def update_index(run_which):
+        """
+        TEAM COPPER ONLY!!!
+        """
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        username = github.get("/user").json()['login']
+        resp = github.get("/user/orgs")
+        if resp.ok:
+    
+            # Only Team Copper members can update the index
+            all_orgs = resp.json()
+            for org in all_orgs:
+                if org['login']=='dcppc':
+                    copper_team_id = '2700235'
+                    mresp = github.get('/teams/%s/members/%s'%(copper_team_id,username))
+                    if mresp.status_code==204:
+                        # Business as usual
+                        UpdateIndexTask(app.config,
+                                        diff_index=False,
+                                        run_which = run_which)
+                        flash("Rebuilding index, check console output")
+                        # This redirects user to /control_panel route
+                        # to prevent accidental re-indexing
+                        return redirect(url_for("control_panel"))
+    
+        return render_template('403.html')
+    
+    
+    @app.route('/control_panel')
+    def control_panel():
+        """
+        TEAM COPPER ONLY!!!
+        """
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        username = github.get("/user").json()['login']
+        resp = github.get("/user/orgs")
+        if resp.ok:
+    
+            # Only Team Copper members can access the control panel
+            all_orgs = resp.json()
+            for org in all_orgs:
+                if org['login']=='dcppc':
+                    copper_team_id = '2700235'
+                    mresp = github.get('/teams/%s/members/%s'%(copper_team_id,username))
+                    if mresp.status_code==204:
+                        # Business as usual
+                        return render_template("controlpanel.html")
+    
+            # Not in dcppc 
+            return render_template('403.html')
+    
+        # Could not reach Github
+        return render_template('404.html')
+    
+    
+    
+    @app.route('/master_list')
+    def master_list():
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        username = github.get("/user").json()['login']
+        resp = github.get("/user/orgs")
+        if resp.ok:
+    
+            # If they are in dcppc, show them masterlist.html
+            # Otherwise, hit em with a 403
+            all_orgs = resp.json()
+            for org in all_orgs:
+                if org['login']=='dcppc':
+                    # Business as usual
+                    return render_template("masterlist.html")
+    
+            # Not in dcppc 
+            return render_template('403.html')
+    
+        # Could not reach Github
+        return render_template('404.html')
+    
+    
+    
+    @app.route('/list/<doctype>')
+    def list_docs(doctype):
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        username = github.get("/user").json()['login']
+        resp = github.get("/user/orgs")
+        if resp.ok:
+            all_orgs = resp.json()
+            for org in all_orgs:
+                if org['login']=='dcppc':
+                    # Business as usual
+                    search = Search(app.config["INDEX_DIR"])
+                    results_list = search.get_list(doctype)
+                    for result in results_list:
+                        if 'created_time' in result.keys():
+                            ct = result['created_time']
+                            result['created_time'] = datetime.strftime(ct,"%Y-%m-%d %I:%M %p")
+                        if 'modified_time' in result.keys():
+                            mt = result['modified_time']
+                            result['modified_time'] = datetime.strftime(mt,"%Y-%m-%d %I:%M %p")
+                        if 'indexed_time' in result.keys():
+                            it = result['indexed_time']
+                            result['indexed_time'] = datetime.strftime(it,"%Y-%m-%d %I:%M %p")
+                    return jsonify(results_list)
+    
+        # nope
+        return render_template('403.html')
 
+
+
+    @app.route('/feedback', methods=['POST'])
+    def parse_request():
+    
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        username = github.get("/user").json()['login']
+        resp = github.get("/user/orgs")
+        if resp.ok:
+            all_orgs = resp.json()
+            for org in all_orgs:
+                if org['login']=='dcppc':
+    
+    
+                    try:
+                        # Business as usual
+                        data = request.form.to_dict();
+                        data['github_login'] = username
+                        data['timestamp'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+                        feedback_database = 'feedback_database.json'
+                        if not os.path.isfile(feedback_database):
+                            with open(feedback_database,'w') as f:
+                                json_data = [data]
+                                json.dump(json_data, f, indent=4)
+    
+                        else:
+                            json_data = []
+                            with open(feedback_database,'r') as f:
+                                json_data = json.load(f)
+    
+                            json_data.append(data)
+    
+                            with open(feedback_database,'w') as f:
+                                json.dump(json_data, f, indent=4)
+    
+                        ## Should be done with Javascript
+                        #flash("Thank you for your feedback!")
+                        return jsonify({'status':'ok','message':'Thank you for your feedback!'})
+                    except:
+                        return jsonify({'status':'error','message':'An error was encountered while submitting your feedback. Try submitting an issue in the <a href="https://github.com/dcppc/centillion/issues/new">dcppc/centillion</a> repository.'})
+    
+    
+        # nope
+        return render_template('403.html')
+    
+    
+    
+    @app.route('/help')
+    def help():
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        username = github.get("/user").json()['login']
+        resp = github.get("/user/orgs")
+        if resp.ok:
+    
+            # If they are in dcppc, show them help
+            # Otherwise, hit em with a 403
+            all_orgs = resp.json()
+            for org in all_orgs:
+                if org['login']=='dcppc':
+                    # Business as usual
+                    with open('pages/help.md','r') as f:
+                        content = Markup(markdown.markdown(f.read()))
+                    return render_template("help.html",**locals())
+    
+            # Not in dcppc 
+            return render_template('403.html')
+    
+        # Could not reach Github
+        return render_template('404.html')
+    
+    
+
+    @app.route('/faq')
+    def faq():
+        if not github.authorized:
+            return redirect(url_for("github.login"))
+        username = github.get("/user").json()['login']
+        resp = github.get("/user/orgs")
+        if resp.ok:
+    
+            # If they are in dcppc, show them faq
+            # Otherwise, hit em with a 403
+            all_orgs = resp.json()
+            for org in all_orgs:
+                if org['login']=='dcppc':
+                    # Business as usual
+                    with open('pages/faq.md','r') as f:
+                        content = Markup(markdown.markdown(f.read()))
+                    return render_template("faq.html",**locals())
+    
+            # Not in dcppc 
+            return render_template('403.html')
+    
+        # Could not reach Github
+        return render_template('404.html')
+
+
+
+    @app.errorhandler(404)
+    def oops(e):
+        return render_template('404.html')
+    
+    
+    def store_search(query, fields):
+        """
+        Store searches in a text file
+        """
+        if os.path.exists(last_searches_file):
+            with codecs.open(last_searches_file, 'r', encoding='utf-8') as f:
+                contents = f.readlines()
+        else:
+            contents = []
+    
+        search = "query=%s&fields=%s\n" % (query, fields)
+        if not search in contents:
+            contents.insert(0, search)
+    
+        # TODO: Fix this to record information more cleanly
+        with codecs.open(last_searches_file, 'w', encoding='utf-8') as f:
+            f.writelines(contents)
 
 
