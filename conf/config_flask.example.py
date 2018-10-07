42bb83dfdfc85061526a6c1a030034caf32ea011
C Reid <charlesreid1@Gmail.com>
2018-10-07 16:18:34 -0700

    update example flask config

--- conf/config_flask.example.py
+++ conf/config_flask.example.py
@@ -34,5 +34,54 @@ TAGLINE = "Search the Data Commons"
 
 # Flask settings
 DEBUG = True
+
+# Flask secret key
 SECRET_KEY = 'XXXXX'
+
+
+
+######################################
+# List of DCPPC repositories to index
+
+REPOSITORIES = [
+        "dcppc/documentation-archive",
+        "dcppc/organize",
+        "dcppc/apis",
+        "dcppc/data-stewards",
+        "dcppc/dcppc-phase1-demos",
+        "dcppc/dcppc-deliverables",
+        "dcppc/dcppc-bot",
+        "dcppc/guids",
+        "dcppc/dcppc.github.io",
+        "dcppc/test-dcppc-deliverables",
+        "dcppc/crosscut-metadata",
+        "dcppc/project-management",
+        "dcppc/dcppc-milestones",
+        "dcppc/dcppc-workshops",
+        "dcppc/2018-may-workshop",
+        "dcppc/internal",
+        "dcppc/private-www",
+        "dcppc/full-stacks",
+        "dcppc/2018-june-workshop",
+        "dcppc/2018-july-workshop",
+        "dcppc/metadata-matrix",
+        "dcppc/design-guidelines",
+        "dcppc/design-guidelines-discuss",
+        "dcppc/lucky-penny",
+        "dcppc/2018-august-workshop",
+        "dcppc/2018-september-workshop",
+        "dcppc/nih-demo-meetings",
+        "dcppc/markdown-issues",
+        "dcppc/2018-paper-dcppc",
+        "dcppc/centillion",
+        "dcppc/public-www",
+        "dcppc/uncle-archie",
+        "dcppc/use-case-library",
+        "dcppc/sodium-documentation",
+        "dcppc/calendars",
+        "dcppc/data-access",
+        "dcppc/four-year-plan",
+        "dcppc/2018-oct-workshop",
+        "dcppc/cloud-guidebook",
+]
 
