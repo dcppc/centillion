0d0ff9cef89ae7d41ce7a3d81ba5eab24aa2d98e
Charles Reid <charlesreid1@gmail.com>
2018-10-24 07:49:34 -0700

    update the main centillion run scripts

--- scripts/run_centillion.py
+++ scripts/run_centillion.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import centillion
+import logging
+import os
 
 """
 Run centillion
@@ -8,6 +10,13 @@ https://search.nihdatacommons.us
 
 Port 5000
 """
+
+log_dir = '/tmp/centillion'
+log_file = os.path.join(log_dir,'centillion.log')
+
+logging.basicConfig(level=logging.INFO,
+                    filename=log_file,
+                    filemode='w')
 
 app = centillion.webapp.get_flask_app()
 app.run(port=5000,debug=False)
--- scripts/run_localcentillion.py
+++ scripts/run_localcentillion.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import centillion
+import logging
 import os
 
 
@@ -11,8 +12,14 @@ https://search.nihdatacommons.us
 Port 5000
 """
 
+#os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 
-os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+log_dir = '/tmp/centillion'
+log_file = os.path.join(log_dir,'localcentillion.log')
+
+logging.basicConfig(level=logging.INFO,
+                    filename=log_file,
+                    filemode='w')
 
 app = centillion.webapp.get_flask_app()
 app.run(port=5000)
