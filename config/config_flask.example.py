59530989201cc4a4151885131df2ad5fe109d4d2
C Reid <charlesreid1@Gmail.com>
2019-02-07 23:10:30 -0800

    update config and gitignore and example to sync

--- config/config_flask.example.py
+++ config/config_flask.example.py
@@ -138,45 +138,7 @@ GITHUB_OAUTH_CLIENT_SECRET = "YYY"
 TRUNCATE_ISSUES_LISTING = False
 
 REPOSITORIES = [
-        "dcppc/documentation-archive",
-        "dcppc/organize",
-        "dcppc/apis",
-        "dcppc/data-stewards",
-        "dcppc/dcppc-phase1-demos",
-        "dcppc/dcppc-deliverables",
-        "dcppc/dcppc-bot",
-        "dcppc/guids",
-        "dcppc/dcppc.github.io",
-        "dcppc/test-dcppc-deliverables",
-        "dcppc/crosscut-metadata",
-        "dcppc/project-management",
-        "dcppc/dcppc-milestones",
-        "dcppc/dcppc-workshops",
-        "dcppc/2018-may-workshop",
-        "dcppc/internal",
-        "dcppc/private-www",
-        "dcppc/full-stacks",
-        "dcppc/2018-june-workshop",
-        "dcppc/2018-july-workshop",
-        "dcppc/metadata-matrix",
-        "dcppc/design-guidelines",
-        "dcppc/design-guidelines-discuss",
-        "dcppc/lucky-penny",
-        "dcppc/2018-august-workshop",
-        "dcppc/2018-september-workshop",
-        "dcppc/nih-demo-meetings",
-        "dcppc/markdown-issues",
-        "dcppc/2018-paper-dcppc",
-        "dcppc/centillion",
-        "dcppc/public-www",
-        "dcppc/uncle-archie",
-        "dcppc/use-case-library",
-        "dcppc/sodium-documentation",
-        "dcppc/calendars",
-        "dcppc/data-access",
-        "dcppc/four-year-plan",
-        "dcppc/2018-oct-workshop",
-        "dcppc/cloud-guidebook",
+        "dcppc/centillion"
 ]
 
 
@@ -189,7 +151,8 @@ GOOGLE_DRIVE_ENABLED = True
 # client API credentials, downloaded from
 # Google Cloud console. This is required to
 # index Google Drive files.
-
+# NOTE: this is different from client_secret.json
+GOOGLE_DRIVE_CREDENTIALS_FILE="credentials.json"
 
 # Flag to indicate whether to truncate the
 # list of documents processed from Google Drive.
