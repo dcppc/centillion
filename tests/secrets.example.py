bd7dcba76e9894c3989f1f41b35ec2df2c9c58cf
C Reid <charlesreid1@Gmail.com>
2019-02-08 21:05:01 -0800

    add example secrets file

---
+++ tests/secrets.example.py
@@ -0,0 +1,5 @@
+GITHUB_OAUTH_CLIENT_ID      = "XXXXX"
+GITHUB_OAUTH_CLIENT_SECRET  = "XXXXX"
+GITHUB_TOKEN                = "XXXXX"
+DISQUS_TOKEN                = "XXXXX"
+SECRET_KEY                  = "XXXXX"
