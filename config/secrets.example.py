13f387bacd3902bef74ae95c18d02aefba4f233f
C Reid <charlesreid1@Gmail.com>
2019-02-08 21:36:49 -0800

    remove secrets example (not using)

--- config/secrets.example.py
+++
@@ -1,5 +0,0 @@
-GITHUB_OAUTH_CLIENT_ID      = "XXXXX"
-GITHUB_OAUTH_CLIENT_SECRET  = "XXXXX"
-GITHUB_TOKEN                = "XXXXX"
-DISQUS_TOKEN                = "XXXXX"
-SECRET_KEY                  = "XXXXX"
