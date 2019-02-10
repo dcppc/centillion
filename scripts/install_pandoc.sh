16e9011d4927d31e4f8b3a7211fb0bfc66f30d37
C Reid <charlesreid1@Gmail.com>
2019-02-10 00:19:37 -0800

    install pandoc too

--- scripts/install_pandoc.sh
+++ scripts/install_pandoc.sh
@@ -12,7 +12,12 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 OFILE="/tmp/pandoc.deb"
-curl -L https://github.com/jgm/pandoc/releases/download/2.2.2.1/pandoc-2.2.2.1-1-amd64.deb -o ${OFILE}
+
+#VERSION="2.2.2.1"
+VERSION="2.6"
+
+curl -L -O https://github.com/jgm/pandoc/releases/download/${VERSION}/pandoc-${VERSION}-1-amd64.deb -o ${OFILE}
+
 dpkg -i ${OFILE}
 rm -f ${OFILE}
 
